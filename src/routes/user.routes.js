16f9b740558e0ff34af485f9525d7b5db52543d8
Syed zain ali shah <137421662+zain-23@users.noreply.github.com>
2023-12-28 17:54:59 +0500

    make route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,6 +8,8 @@ import {
   refreshAccessToken,
   registerUser,
   updateUserAvatar,
+  updateUserCoverImage,
+  updateUserDetails,
 } from "../controllers/user.contoller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -35,7 +37,13 @@ router.route("/change-password").post(verifyJwt, changeCurrentPassword);
 
 router
   .route("/change-avatar")
-  .post(verifyJwt, upload.single("avatar"), updateUserAvatar);
+  .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/change-coverimage")
+  .patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+router.route("/update-user").patch(verifyJwt, updateUserDetails);
+
+// later i move this from here
 router.route("/subscribed/:username").post(verifyJwt, channelSubscribed);
 router.route("/find-channel/:userName").post(getUserChannelsDetails);
 
