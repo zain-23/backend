91a1b03c5d53a55b1449008f45ddcae3669b1161
Syed zain ali shah <137421662+zain-23@users.noreply.github.com>
2023-12-30 00:25:35 +0500

    create a comments schema

---
+++ src/models/comments.model.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const COMMENTS = mongoose.model("Comment", commentSchema);
