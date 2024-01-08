47dccb4d847304cf48991b64eb69506439492893
Syed zain ali shah <137421662+zain-23@users.noreply.github.com>
2024-01-09 00:29:24 +0500

    created all controller of comment and one controller of video

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,31 +1,158 @@
-import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
 
-})
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(401, "can't find video");
+  }
+  const skip = (page - 1) * limit;
+  const videoComments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "user",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              fullName: 1,
+              email: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        user: {
+          $first: "$user",
+        },
+      },
+    },
+    {
+      $skip: skip,
+    },
+    {
+      $limit: limit,
+    },
+  ]);
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(200, "get Video Comments Successfully", videoComments)
+    );
+});
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
-})
+  // TODO: add a comment to a video
+  const { content } = req.body;
+  const { videoId } = req.params;
+
+  if (!content) {
+    throw new ApiError(401, "message is required");
+  }
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(401, "can't find video");
+  }
+
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user?._id,
+  });
+
+  if (!comment) {
+    throw new ApiError(
+      500,
+      "something went while post the comment try again later"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, "Comment posted successfully", comment));
+});
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
-})
+  // TODO: update a comment
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(401, "message is required");
+  }
+
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(401, "can't find comment");
+  }
+
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: {
+        content,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedComment) {
+    throw new ApiError(
+      500,
+      "something went while update the comment try again later"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, "Comment updated successfully", updatedComment));
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
-})
+  // TODO: delete a comment
+  const { commentId } = req.params;
 
-export {
-    getVideoComments, 
-    addComment, 
-    updateComment,
-     deleteComment
-    }
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(401, "can't find comment");
+  }
+
+  const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+  if (!deletedComment) {
+    throw new ApiError(
+      500,
+      "something went while deleted the comment try again later"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        201,
+        "Comment deletedComment successfully",
+        deletedComment
+      )
+    );
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,47 +1,96 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
-})
+  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+  //TODO: get all videos based on query, sort, pagination
+});
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
-    // TODO: get video, upload to cloudinary, create video
-})
+  const { title, description } = req.body;
+
+  if (
+    [title, description].some((fields) => fields === "" || fields === undefined)
+  ) {
+    throw new ApiError(401, "Title and Description is required");
+  }
+
+  let videoFileLocalPath;
+  let thumbnailFileLocalPath;
+
+  if (
+    req.files &&
+    Array.isArray(req.files.videoFile) &&
+    Array.isArray(req.files.thumbnail)
+  ) {
+    videoFileLocalPath = req.files?.videoFile[0].path;
+    thumbnailFileLocalPath = req.files?.thumbnail[0].path;
+  }
+
+  if (!videoFileLocalPath && !thumbnailFileLocalPath) {
+    throw new ApiError(401, "Thumbanil and video is required");
+  }
+
+  const video = await uploadOnCloudinary(videoFileLocalPath);
+  const thumbnail = await uploadOnCloudinary(thumbnailFileLocalPath);
+
+  if (!video) {
+    throw new ApiError(500, "Some thing went wrong while uploading video");
+  }
+  if (!thumbnail) {
+    throw new ApiError(500, "Some thing went wrong while uploading thumbnail");
+  }
+
+  const publishedVideo = await Video.create({
+    title,
+    description,
+    video: video.url,
+    thumbnail: thumbnail.url,
+    duration: Math.round(video.duration),
+    owner: req.user?._id,
+  });
+
+  if (!publishedVideo) {
+    throw new ApiError(
+      500,
+      "Some thing went wrong while publishing video try again later"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, "video published successfully", publishedVideo));
+});
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: get video by id
-})
+  const { videoId } = req.params;
+  //TODO: get video by id
+});
 
 const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: update video details like title, description, thumbnail
-
-})
+  const { videoId } = req.params;
+  //TODO: update video details like title, description, thumbnail
+});
 
 const deleteVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: delete video
-})
+  const { videoId } = req.params;
+  //TODO: delete video
+});
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-})
+  const { videoId } = req.params;
+});
 
 export {
-    getAllVideos,
-    publishAVideo,
-    getVideoById,
-    updateVideo,
-    deleteVideo,
-    togglePublishStatus
-}
+  getAllVideos,
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+};
